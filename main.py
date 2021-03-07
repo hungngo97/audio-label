ba68863122b40f0f7eb373160f16acc02f6c2e56
Hung Ngo <hung.ngovinh97@gmail.com>
2021-03-06 18:35:52 -0800

    Added default option and not allow to export if not chosen label

--- main.py
+++ main.py
@@ -6,17 +6,21 @@ import random
 from os import listdir
 from os.path import isfile, join
 import SessionState
+import copy
 
 answers = {}
+DEFAULT_OPTION = "Choose one option below"
 
-def audio_section(file_name):
-    print('file name', file_name)
+def audio_section(i, file_name):
+    # print('file name', file_name)
     audio_file = open(file_name, 'rb')
     audio_bytes = audio_file.read()
     st.audio(audio_bytes)
-    label = st.radio("Audio label", LABELS, key=file_name)
+    options = copy.deepcopy(random_labels)
+    options.insert(0, DEFAULT_OPTION)
+    label = st.radio("Audio label", options, key=file_name)
     st.write('You selected label: ' + label + ' for the above sound')
-    answers[file_name] = [label]
+    answers[i] = [file_name, label]
 
 
 def get_table_download_link(df):
@@ -33,7 +37,7 @@ def render_trial_sound(label, trials_per_class):
     # read the files from the label directory
     label_dir = './data/' + label
     wav_files = [f for f in listdir(label_dir) if isfile(join(label_dir, f))]
-    print('Wav files', wav_files)
+    # print('Wav files', wav_files)
     # render samples sounds
     random_wav_files = random.sample(wav_files, trials_per_class)
     for wav_file in random_wav_files:
@@ -54,15 +58,31 @@ def get_random_prediction_files(labels, trials_per_class):
             result.append(random_wav_file)
     return result
 
+def render_download_link():
+    # for loop through answers, if there's an answer that is unchosen, then do not let the user export answer
+    is_complete = True
+    for i in answers:
+        chosen_label = answers[i][1]
+        if chosen_label == DEFAULT_OPTION:
+            st.markdown('*Question {} is not chosen*'.format(i))
+            is_complete = False
+            continue
+    if (not is_complete):
+        st.markdown('**Cannot export to csv because still miss question(s) above**')
+        return
+    if (is_complete):
+        st.markdown(get_table_download_link(pd.DataFrame.from_dict(answers, orient='index',
+                        columns=['file_name', 'chosen_label'])), unsafe_allow_html=True)
 
+  
 DATA_SOUNDS = [x[0] for x in os.walk('./data')]
 LABELS = []
 for datasound in DATA_SOUNDS:
     if len(datasound) <= len('./data'):
         continue
     LABELS.append(datasound[len('./data/'):])
-print(DATA_SOUNDS)
-print(LABELS)
+# print(DATA_SOUNDS)
+# print(LABELS)
 
 SOUND_CLASSES = 5
 TRIAL_SAMPLES_PER_CLASS = 5
@@ -74,7 +94,7 @@ random_labels = session.random_labels
 if len(random_labels) <= 0:
     random_labels = random.sample(LABELS, SOUND_CLASSES)
 session.random_labels = random_labels
-print('Random labels', random_labels)
+# print('Random labels', random_labels)
 
 # Draw sample classes out for users
 st.markdown('**First, you need to get familiar with the sample sounds of what you are going to label**')
@@ -91,14 +111,14 @@ if (len(random_prediction_files) <= 0):
     random_prediction_files = get_random_prediction_files(random_labels, PREDICTION_SAMPLES_PER_CLASS)
     random.shuffle(random_prediction_files)
 session.random_prediction_files = random_prediction_files
-for f in random_prediction_files:
-    st.markdown('**Audio Sample, please listen and answer in the multiple choice below: **')
-    audio_section(f)
+for i, f in enumerate(random_prediction_files):
+    st.markdown('**Audio Sample No.{}, please listen and answer in the multiple choice below: **'.format(i))
+    audio_section(i, f)
 
 
 st.markdown('**After finished labeling, please click the following button to export CSV results and send it to researchers**')
-st.markdown(get_table_download_link(pd.DataFrame.from_dict(answers, orient='index',
-                       columns=['chosen_label'])), unsafe_allow_html=True)
+
+render_download_link()
 
 st.markdown("**Click reset below to reset all of your choices and give a new set of audio files**")
 if st.button("Reset"):
